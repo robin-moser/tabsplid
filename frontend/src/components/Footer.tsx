23b728bf912fbcc9e59e9c6acb1186e28d3cfbb0
Robin Moser <mail@robinmoser.de>
2025-01-26 20:06:22 +0100

    fix footer styling on small screens

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,13 +5,13 @@ const Footer: React.FC = () => {
   const {t} = useTranslation(['common']);
 
   return (
-    <footer className="flex items-center justify-between px-4 py-6 mt-auto
-      bg-neutral-200 dark:bg-dark-600 text-gray-500">
+    <footer className="flex items-center justify-between flex-col sm:flex-row
+      px-4 py-6 mt-auto bg-neutral-200 dark:bg-dark-600 text-gray-500">
       <div className="flex-1"></div>
-      <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} tabsplid. {t('common:allRightsReserved')}</p>
+      <div className="mx-auto text-center relative">
+        <span>&copy; {new Date().getFullYear()} tabsplid. {t('common:allRightsReserved')}</span>
       </div>
-      <div className="text-right flex-1 text-gray-400 dark:text-gray-600">
+      <div className="text-right flex-1 min-w-24 text-gray-400 dark:text-gray-600">
         <span>{t('common:version')} {import.meta.env.VITE_APP_VERSION}</span>
       </div>
     </footer>
